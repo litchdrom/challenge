bf9a7ed7ee7166dc67dc2b18e23122ed4c84db45
litchdrom <litchdrome@gmail.com>
2022-05-11 16:04:33 +0300

    doubletm fix

--- httpproxy1.py
+++ httpproxy1.py
@@ -4,27 +4,52 @@ from requests import get
 import re
 
 SITE_NAME = 'https://news.ycombinator.com/'
+HTTPLST =['http','https']
 additions = ['gif', 'png', 'ico', 'js', 'css']
-forumparts = ['submit', 'points', 'Search:', 'parent', 'minute']
+#forumparts = ['submit', 'points', 'Search:', 'parent', 'minute']
+forumparts = ['Search:']
 tm = '&trade;'
 
+def tmreplace(replacetm):
+    if '™' in replacetm:
+        tmartefact = replacetm.replace('™', '')
+        print("!!!!!!!!!!!!!!!!!!!!!", replacetm.replace('™', ''))
+        return tmartefact
+    else:
+        return replacetm
 
 def replacer(repl):
     templist = repl.split()
     for i, a in enumerate(templist):
         if re.search(r'\b[a-zA-ZА-Яа-я]{6}\b', a) and a not in forumparts:
-            templist[i] = a + tm
-            if a[len(a) - 2:] == '),':
-                templist[i] = a[:-2] + tm + '),'
-            if len(a.split('.')) > 1:
-                templist[i] = a
-            if re.search(r'\W', a[-1]):
-                symb = a[-1]
-                templist[i] = a[:-1] + tm + symb
-            if len(a.split('-')) > 1 or len(a.split('\'')) > 1:
-                templist[i] = a
-            if len(a.split('`')) > 1:
-                templist[i] = a
+            print(a)
+            a=tmreplace(a)
+
+            res = re.findall(r'\w+', a)
+            print(res,'###')
+            if len(res) > 1:
+                if not res[0] in HTTPLST:
+                    for findsix in res:
+                        print(findsix,'$$$')
+                        if len(findsix) == 6:
+                            templist[i] = a.replace(findsix,findsix+tm)
+            else:
+                templist[i] = a.replace(res[0],res[0]+tm)
+
+#            if a[len(a) - 2:] == '),':
+#                templist[i] = a[:-2] + tm + '),'
+#            if len(a.split('.')) > 1:
+#                templist[i] = a
+##            if re.search(r'\W', a[-1]):
+#                symb = a[-1]
+#                templist[i] = a[:-1] + tm + symb
+#            if len(a.split('-')) > 1 or len(a.split('\'')) > 1:
+#                templist[i] = a
+#            if len(a.split('`')) > 1:
+#                templist[i] = a
+#            if re.search(tm, a):
+#                templist[i] = a+'shit'
+
 
     listtostr = ' '.join(map(str, templist))
     return listtostr
